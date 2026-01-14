85169aec967f57c16e310e51254eb12bc50094da
KaiFeng-tp-mbap <2404747D@student.tp.edu.sg>
2026-01-14 10:56:34 +0800

    update on fweb project

---
+++ FWEB_project_part3/project/routes/books.js
@@ -0,0 +1,250 @@
+// Import the Express framework
+import express from "express";
+// Create a new router instance to define API routes
+const router = express.Router();
+// import Book from "../models/book.js";
+// POST /book -> Create a new task
+/**
+* @swagger
+* /book:
+*   post:
+*       summary: Create a new book
+*       description: Creates a new book in the database
+*       requestBody:
+*           required: true
+*           content:
+*               application/json:
+*                   schema:
+*                       type: object
+*                       properties:
+*                           title:
+*                               type: string
+*                           completed:
+*                               type: boolean
+*       responses:
+*           201:
+*               description: Task created successfully
+*/
+router.post("/", async (req, res) => {
+    // try {
+        // TODO: Get task data from req.body
+        // const { title, Language,Description,author,published_date,Availability_id, reservationid,Reservation_availablity, Location_id} = req.body;
+        // TODO: Validate the task data
+        // if (!title) {
+        //     return res.status(400).json({ message: "Title is required"
+        //     });
+        // }
+        // TODO: Save the new task to the database
+        // const newBook = new Task({
+        //     title,
+        //     Language,
+        //     Description,
+        //     author,
+        //     published_date,
+        //     Availability_id,
+        //     reservationid,
+        //     Reservation_availablity,
+        //     Location_id
+        // });
+        // TODO: Return a success response or error
+        // const savedBook = await newBook.save();
+
+        // Return success response
+    //     res.status(201).json(savedBook);
+        res.json({
+            message: "POST /book - Create a new book endpoint hit successfully",
+        });
+    // } catch (error) {
+    //     console.error(error);
+    //     res.status(500).json({ message: "Server error" });
+    // }
+});
+
+// GET /book -> Retrieve all book
+/**
+* @swagger
+* /book:
+*   get:
+*       summary: Retrieve all book
+*       description: Returns a list of all book
+*       responses:
+*           200:
+*               description: A list of book
+*               content:
+*                   application/json:
+*                       schema:
+*                           type: array
+*                           items:
+*                               type: object
+*                           properties:
+*                               id:
+*                                   type: string
+*                               title:
+*                                   type: string
+*                               completed:
+*                                   type: boolean
+*/
+router.get("/", async (req, res) => {
+//    try {
+    //  TODO: Fetch all tasks from the database and populate the "items" field
+    //  const books = await Task.find().populate("items");
+    //  res.status(200).json(tasks);// TODO: Return tasks as JSON
+     
+     res.json({
+         message: "GET /book - Retrieve all tasks endpoint hit successfully",
+     });
+//    } catch (error) {
+//     console.error(error);
+//    }
+});
+
+// GET /tasks/:id -> Retrieve a task by ID
+/**
+* @swagger
+* /tasks/{id}:
+*   get:
+*       summary: Retrieve a specific task by ID
+*       description: Returns a single task based on the provided ID
+*       parameters:
+*           - in: path
+*             name: id
+*             schema:
+*                 type: string
+*             required: true
+*
+*       responses:
+*           200:
+*               description: A single task
+*               content:
+*                   application/json:
+*                       schema:
+*                           type: object
+*                           properties:
+*                               id:
+*                                   type: string
+*                               title:
+*                                   type: string
+*                               completed:
+*                                   type: boolean
+*/
+router.get("/:id", async (req, res) => {
+//    try {
+//      // TODO: Get task ID from req.params.id
+//      const taskId = req.params.id;
+ 
+//      // TODO: Fetch the specific task from the database
+//      const task = await Task.findById(taskId).populate("items");
+ 
+//      // TODO: Return the task or an error if not found
+//      if (!task) {
+//      return res.status(404).json({ message: "Task not found" });
+//      }
+//      // Return the task
+//      res.json(task);
+     res.json({
+         message: `GET /book/${req.params.id} - Retrieve a specific task endpoint hit successfully`,
+     });
+//    } catch (error) {
+//     res.status(500).json({ message: "Server error" });
+//    }
+
+})
+
+// PUT /tasks/:id -> Update a task's details
+/**
+* @swagger
+* /tasks/{id}:
+*   put:
+*       summary: Update a specific task by ID
+*       description: Updates the details of a task based on the provided ID
+*       parameters:
+*           - in: path
+*             name: id
+*             schema:
+*                 type: string
+*             required: true
+*       requestBody:
+*           required: true
+*           content:
+*               application/json:
+*                   schema:
+*                       type: object
+*                       properties:
+*                           title:
+*                               type: string
+*                           completed:
+*                               type: boolean
+*       responses:
+*           200:
+*               description: Task updated successfully
+*/
+router.put("/:id", async (req, res) => {
+    // try {
+    //     // TODO: Get task ID from req.params.id
+    //     const bookId = req.params.id;
+    //     // TODO: Get updated data from req.body
+    //     const updatedData = req.body;
+    //     // TODO: Update the task in the database
+    //     const updatedBook = await Task.findByIdAndUpdate(
+    //     bookId,
+    //     updatedData,
+    //     { new: true, runValidators: true }// TODO: Return the updated book or an error
+        
+    //     );
+    //     // If book not found, return 404
+    //     if (!updatedBook) {
+    //     return res.status(404).json({ message: "book not found"
+    //     });
+    //     }
+    //     // Return the updated task
+    //     res.json(updatedTask);
+
+        res.json({
+            message: `PUT /tasks/${req.params.id} - Update book endpoint hit successfully`,
+        });
+    // } catch (error) {
+    //     console.error(error);
+    //     res.status(500).json({ message: "Server error" });
+    // }
+});
+
+// DELETE /tasks/:id -> Delete a task
+/**
+* @swagger
+* /tasks/{id}:
+*   delete:
+*       summary: Delete a specific task by ID
+*       description: Deletes a task from the database based on the provided ID
+*       parameters:
+*           - in: path
+*             name: id
+*             schema:
+*                 type: string
+*             required: true
+*       responses:
+*           200:
+*               description: Task deleted successfully
+*/
+router.delete("/:id", async (req, res) => {
+    // try {
+        // TODO: Get task ID from req.params.id
+        // const bookId = req.params.id;
+        // // TODO: Remove the task from the database
+        // const deletedBook = await Task.findByIdAndDelete(bookId);
+        // // TODO: Return a success message or error if not found
+        // if (!deletedBook) {
+        //     return res.status(404).json({ message: "book not found"
+        //     });
+        // }
+        // // Return success message
+        // res.json({ message: "book deleted successfully", task:deletedBook });
+        res.json({
+            message: `DELETE /book/${req.params.id} - Delete task endpoint hit successfully`,
+        });
+    // } catch (error) {
+    //     console.error(error);
+    //     res.status(500).json({ message: "Server error" });
+    // }
+})
+
+export default router;
---
+++ FWEB_project_part3/project/server.js
@@ -0,0 +1,60 @@
+import express from "express";
+import cors from "cors";
+
+// Import and use the task routes defined in routes/task.js
+import books from "./routes/books.js";
+
+// Import Swagger UI libraries
+import swaggerUi from "swagger-ui-express";
+import swaggerJsdoc from "swagger-jsdoc";
+
+// import dotenv from "dotenv";
+// import mongoose from "mongoose";
+
+// MongoDB Connection
+// Access MongoDB URI from the environment variable
+
+// dotenv.config(); // Load environment variables from .env
+
+// mongoose
+// .connect(process.env.MONGO_URI)
+// .then(() => console.log("MongoDB Connected"))
+// .catch((err) => console.log(err));
+
+
+// Define swagger options
+const swaggerOptions = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "My API",
+            version: "1.0.0",
+            description: "API documentation for my project",
+        },
+    },
+    apis: ["./routes/*.js"], // Path to your API route files
+};
+const app = express();
+// Enables CORS so your front-end can access your backend API without browser blocking it.
+app.use(cors());
+// Allows Express to parse JSON data from incoming requests
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Mount all the task-related API endpoints under a specific base path
+app.use("/book", books);
+// Initial route to test if your backend server is running properly
+app.get("/", async (req, res) => {
+res.send("<h1>Welcome to my API! The server is running successfully.</h1>");
+});
+// Set port
+const PORT = process.env.PORT || 5050;
+// Start server
+app.listen(PORT, () => {
+console.log(`Server running on http://localhost:${PORT}`);
+});
+
+// Generate swagger documentation
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
+// // Use app.use() to mount the Swagger UI to /api-docs:
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
